e02e3e95a52925109c2f52d448c8a1dc14b333d0
danielev04 <daniele@vottero.eu>
2019-02-08 09:25:10 +0000

    removed old code

--- trafficman.tf
+++ trafficman.tf
@@ -61,12 +61,3 @@ data "null_data_source" "waf-pip" {
   }
   depends_on = ["module.waf"]
 }
-
-# resource "azurerm_traffic_manager_endpoint" "endpoint" {
-#   name                = "${local.backend_name}"
-#   resource_group_name = "${var.product}-shared-infrastructure-${var.env}"
-#   profile_name        = "${var.product}-${var.env}"
-#   target              = "${data.null_data_source.waf-pip.outputs["pip"]}"
-#   type                = "externalEndpoints"
-#   weight              = 100
-# }
