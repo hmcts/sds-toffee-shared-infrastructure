b27370090215ee25ccd8f711e530e491ef9fbcea
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-07-14 16:02:51 +0100

    remove mi

--- vault.tf
+++ vault.tf
@@ -1,15 +1,5 @@
 //KEY VAULT RESOURCE
 
-resource "azurerm_user_assigned_identity" "plum-identity" {
-
-  resource_group_name = "managed-identities-${var.env}-rg"
-  location            = "${var.location}"
-
-  name = "${var.product}-${var.env}-mi"
-
-  tags = "${var.common_tags}"
-}
-
 module "vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "${local.vault_name}"
@@ -20,13 +10,9 @@ module "vault" {
   resource_group_name     = "${azurerm_resource_group.shared_resource_group.name}"
   product_group_object_id = "300e771f-856c-45cc-b899-40d78281e9c1"
   common_tags = "${local.tags}"
-  managed_identity_object_ids= ["${azurerm_user_assigned_identity.plum-identity.principal_id}","${var.managed_identity_object_id}"]
+  managed_identity_object_ids= ["${var.managed_identity_object_id}"]
 }
 
 output "vaultName" {
   value = "${module.vault.key_vault_name}"
-}
-
-output "team_identity_client_id" {
-  value = "${azurerm_user_assigned_identity.plum-identity.client_id}"
 }
