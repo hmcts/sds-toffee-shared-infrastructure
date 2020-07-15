76a20e410e44c01e563b5eec63ac677da38ddc0e
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-07-15 09:21:28 +0100

    Create MI for keyvault (#21)

--- vault.tf
+++ vault.tf
@@ -1,16 +1,17 @@
 //KEY VAULT RESOURCE
 
 module "vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = "${local.vault_name}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  tenant_id               = "${var.tenant_id}"
-  object_id               = "${var.jenkins_AAD_objectId}"
-  resource_group_name     = "${azurerm_resource_group.shared_resource_group.name}"
-  product_group_object_id = "300e771f-856c-45cc-b899-40d78281e9c1"
-  common_tags = "${local.tags}"
-  managed_identity_object_ids= ["${var.managed_identity_object_id}"]
+  source                      = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                        = "${local.vault_name}"
+  product                     = "${var.product}"
+  env                         = "${var.env}"
+  tenant_id                   = "${var.tenant_id}"
+  object_id                   = "${var.jenkins_AAD_objectId}"
+  resource_group_name         = "${azurerm_resource_group.shared_resource_group.name}"
+  product_group_object_id     = "300e771f-856c-45cc-b899-40d78281e9c1"
+  common_tags                 = "${local.tags}"
+  managed_identity_object_ids = ["${var.managed_identity_object_id}"]
+  create_managed_identity     = true
 }
 
 output "vaultName" {
