d66aca8795219ef09c1c183b3d4ac7efb59240c2
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-09-11 10:21:39 +0100

    Upgrade Terraform. Migrate keyvault module to v2.

--- main.tf
+++ main.tf
@@ -1,27 +1,23 @@
-
-
-
 provider "azurerm" {
-  
-  version = "=1.33.1"
+  features {}
 }
 
 locals {
   vault_name = "${var.product}si-${var.env}"
   
-  tags = "${merge(
+  tags = merge(
     var.common_tags,
     map("Team Contact", var.team_contact)
-  )}"
+  )
 
 }
 
 //SHARED RESOURCE GROUP
 resource "azurerm_resource_group" "shared_resource_group" {
   name     = "${var.product}-shared-infrastructure-${var.env}"
-  location = "${var.location}"
+  location = var.location
 
-  tags = "${local.tags}"
+  tags = local.tags
 }
 
 
