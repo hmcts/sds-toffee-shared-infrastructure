56f5659f57ba115eca207678b27fd069c41798fe
Thomas Thornton <>
2021-01-12 13:33:45 +0000

    update to correct object ID

--- vault.tf
+++ vault.tf
@@ -8,7 +8,7 @@ module "vault" {
   tenant_id                   = var.tenant_id
   object_id                   = var.jenkins_AAD_objectId
   resource_group_name         = azurerm_resource_group.shared_resource_group.name
-  product_group_object_id     = "c36eaede-a0ae-4967-8fed-0a02960b1370"
+  product_group_object_id     = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
   common_tags                 = local.tags
   managed_identity_object_id = var.managed_identity_object_id
   create_managed_identity     = true
