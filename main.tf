1a892fed792a383f85898583926d9dcc5f87bcff
mokainos <72137340+mokainos@users.noreply.github.com>
2021-08-31 11:35:31 +0100

    updated tags

--- main.tf
+++ main.tf
@@ -4,11 +4,6 @@ provider "azurerm" {
 
 locals {
   vault_name = "${var.product}si-${var.env}"
-  
-  tags = merge(
-    var.common_tags,
-    map("Team Contact", var.team_contact)
-  )
 
 }
 
@@ -17,7 +12,14 @@ resource "azurerm_resource_group" "shared_resource_group" {
   name     = "${var.product}-shared-infrastructure-${var.env}"
   location = var.location
 
-  tags = local.tags
+  tags = module.tags.common_tags
 }
 
 
+
+module "tags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = lower(var.env)
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
--- vault.tf
+++ vault.tf
@@ -9,7 +9,7 @@ module "vault" {
   object_id                   = var.jenkins_AAD_objectId
   resource_group_name         = azurerm_resource_group.shared_resource_group.name
   product_group_object_id     = "e7ea2042-4ced-45dd-8ae3-e051c6551789"
-  common_tags                 = local.tags
+  common_tags                 = module.tags.common_tags
   managed_identity_object_id = var.managed_identity_object_id
   create_managed_identity     = true
   developers_group            = var.developers_group
