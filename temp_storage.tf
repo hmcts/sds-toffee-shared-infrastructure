ba79383c748083b50067f31ccd8cf5f2e2830e29
msl8r <72858053+msl8r@users.noreply.github.com>
2021-11-02 11:56:56 +0000

    Only build temp storage in staging  (#15)
    * Use branch with environment name for tagging changes
    
    * DTSPO-4155 - temporarily removing storage account as it's breaking the pipeline for other envs
    
    * Update Jenkinsfile_parameterized
    
    * DTSPO-4155 - only build temp storage in staging
    
    * Update Jenkinsfile_CNP
    
    * Update Jenkinsfile_parameterized

--- temp_storage.tf
+++ temp_storage.tf
@@ -1,4 +1,6 @@
 module "storage_account" {
+  count                    = var.env == "stg" ? 1 : 0
+  
   source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   env                      = var.env
   storage_account_name     = "testblobstoragetov2"
