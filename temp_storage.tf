652b19c27cbab6c3b379e8b343301554ba29c81a
msl8r <72858053+msl8r@users.noreply.github.com>
2021-11-02 11:19:12 +0000

    DTSPO-4155 - only build temp storage in staging

--- temp_storage.tf
+++ temp_storage.tf
@@ -1,11 +1,13 @@
-# module "storage_account" {
-#   source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-#   env                      = var.env
-#   storage_account_name     = "testblobstoragetov2"
-#   resource_group_name      = azurerm_resource_group.shared_resource_group.name
-#   location                 = var.location
-#   account_kind             = "StorageV2"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   access_tier              = "Hot"
-# }
+module "storage_account" {
+  count                    = var.env == "stg" ? 1 : 0
+  
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = var.env
+  storage_account_name     = "testblobstoragetov2"
+  resource_group_name      = azurerm_resource_group.shared_resource_group.name
+  location                 = var.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
+}
