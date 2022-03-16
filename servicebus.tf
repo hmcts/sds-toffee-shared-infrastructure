8636d8bad903915ac22f81736252a0ac18a5094f
msl8r <72858053+msl8r@users.noreply.github.com>
2022-03-16 15:06:41 +0000

    Update servicebus.tf

--- servicebus.tf
+++ servicebus.tf
@@ -4,13 +4,12 @@ locals {
 
 module "servicebus-namespace" {
   source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
-#   source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_private_endpoint"
   name                    = local.servicebus_namespace_name
-#   project                 = var.project
   location                = var.location
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
   env                     = var.env
   common_tags             = local.tags
+#   project                 = var.project
 #   capacity                = 1
 #   enable_private_endpoint = var.servicebus_enable_private_endpoint
 }
