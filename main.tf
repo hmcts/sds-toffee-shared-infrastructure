92590f3dfe1210fc5f5b2ce594a59971c3314be9
Louise Huyton <l.huyton@kainos.com>
2022-03-24 13:42:34 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "shared_resource_group" {
   name     = "${var.product}-shared-infrastructure-${var.env}"
   location = var.location
 
-  tags = local.tags
+  tags = var.tags
 }
 
 
