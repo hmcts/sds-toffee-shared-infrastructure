37496ebb4ebadb04e8e2a230b36b4f47ba6df357
msl8r <72858053+msl8r@users.noreply.github.com>
2022-03-25 12:18:27 +0000

    Move service bus module to master branch

--- servicebus.tf
+++ servicebus.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 module "servicebus-namespace" {
-  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_private_endpoint"
+  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
   name                    = local.servicebus_namespace_name
   location                = var.location
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
