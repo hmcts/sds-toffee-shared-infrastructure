bbe9efa277db1b1f91d52446c779dcfc853fa2ac
msl8r <matt.slater@hmcts.net>
2022-03-29 20:31:00 +0100

    DTSPO-6371 - deploy standard service bus

--- servicebus.tf
+++ servicebus.tf
@@ -3,14 +3,14 @@ locals {
 }
 
 module "servicebus-namespace" {
-  source                           = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_deploy_to_vnet_rg"
-  name                             = local.servicebus_namespace_name
-  location                         = var.location
-  resource_group_name              = azurerm_resource_group.shared_resource_group.name
-  env                              = var.env
-  common_tags                      = var.common_tags
-  project                          = var.project
-  capacity                         = 1
-  enable_private_endpoint          = true
-  private_endpoint_subscription_id = var.subscription_id
+  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_deploy_to_vnet_rg"
+  name                = local.servicebus_namespace_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.shared_resource_group.name
+  env                 = var.env
+  common_tags         = var.common_tags
+  project             = var.project
+  # capacity                         = 1
+  enable_private_endpoint = false
+  # private_endpoint_subscription_id = var.subscription_id
 }
