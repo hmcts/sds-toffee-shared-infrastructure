90878f9175b6702ad0afaa86364c5fa90308e780
msl8r <72858053+msl8r@users.noreply.github.com>
2022-03-30 12:55:10 +0100

    Update servicebus.tf

--- servicebus.tf
+++ servicebus.tf
@@ -3,16 +3,16 @@ locals {
 }
 
 module "servicebus-namespace" {
-  providers = {
-    azurerm.private-endpoint = azurerm
-  }
+#   providers = {
+#     azurerm.private-endpoint = azurerm
+#   }
   source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-6371_remove_provider"
   name                    = local.servicebus_namespace_name
   location                = var.location
   resource_group_name     = azurerm_resource_group.shared_resource_group.name
   env                     = var.env
   common_tags             = var.common_tags
-  project                 = var.project
-  capacity                = 1
-  enable_private_endpoint = true
+#   project                 = var.project
+#   capacity                = 1
+#   enable_private_endpoint = true
 }
