f248899abe436520247883f51ce88911eff2dded
msl8r <matt.slater@hmcts.net>
2022-03-30 12:10:56 +0100

    DTSPO-6371 - tidy provider

--- state.tf
+++ state.tf
@@ -16,9 +16,3 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "private-endpoint"
-  subscription_id            = var.private_endpoint_subscription_id
-}
