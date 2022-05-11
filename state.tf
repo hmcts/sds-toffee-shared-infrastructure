7ddd3d72d07f84e180d39292f6fbde3b3138c4bf
msl8r <72858053+msl8r@users.noreply.github.com>
2022-05-11 17:11:24 +0100

    DTSPO-6371 - fix variable name

--- state.tf
+++ state.tf
@@ -20,5 +20,5 @@ provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "private_endpoint"
-  subscription_id            = var.aks_subscription_id
+  subscription_id            = var.subscription_id
 }
