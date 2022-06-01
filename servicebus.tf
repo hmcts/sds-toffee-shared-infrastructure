c3a48cb6ae9ea06e54615a11c28808578579d899
msl8r <matt.slater@hmcts.net>
2022-06-01 12:12:25 +0100

    DTSPO-8169 - add Azure Service Bus Data Owner role assignment to toffe mi

--- servicebus.tf
+++ servicebus.tf
@@ -6,7 +6,7 @@ module "servicebus-namespace" {
   providers = {
     azurerm.private_endpoint = azurerm.private_endpoint
   }
-  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=DTSPO-8169_resource_id_output"
   name                    = local.servicebus_namespace_name
   location                = var.location
   sku                     = "Basic"
@@ -15,4 +15,14 @@ module "servicebus-namespace" {
   common_tags             = var.common_tags
   project                 = var.project
 }
-	
+
+data "azurerm_user_assigned_identity" "toffee_mi" {
+  name                = "toffee-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+resource "azurerm_role_assignment" "toffee_servicebus" {
+  principal_id = data.azurerm_user_assigned_identity.toffee_mi
+  scope        = module.servicebus-namespace.servicebus_namespace_id
+  role_definition_name = "Azure Service Bus Data Owner"
+}
