4377cf5cf785d8f0a4e54977be3173c8ebb76f93
msl8r <72858053+msl8r@users.noreply.github.com>
2022-06-01 15:36:43 +0100

    DTSPO-8169 - create basic service bus for recipe receiver app (#24)
    * DTSPO-8169 - create standard service bus for recipe receiver app
    
    * DTSPO-8169 - add Azure Service Bus Data Owner role assignment to toffe mi
    
    * DTSPO-8169 - revert back to master
    
    * DTSPO-8169 - fix reference to sb resource id
    
    * DTSPO-8169 - give keda permission to read from service bus
    
    * DTSPO-8169 - fix errors

---
+++ servicebus.tf
@@ -0,0 +1,39 @@
+locals {
+  servicebus_namespace_name = "${var.product}-servicebus-${var.env}"
+}
+
+module "servicebus-namespace" {
+  providers = {
+    azurerm.private_endpoint = azurerm.private_endpoint
+  }
+  source                  = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
+  name                    = local.servicebus_namespace_name
+  location                = var.location
+  sku                     = "Basic"
+  resource_group_name     = azurerm_resource_group.shared_resource_group.name
+  env                     = var.env
+  common_tags             = var.common_tags
+  project                 = var.project
+}
+
+data "azurerm_user_assigned_identity" "toffee" {
+  name                = "toffee-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+resource "azurerm_role_assignment" "toffee_servicebus_data_owner" {
+  principal_id = data.azurerm_user_assigned_identity.toffee.id
+  scope        = module.servicebus-namespace.id
+  role_definition_name = "Azure Service Bus Data Owner"
+}
+
+data "azurerm_user_assigned_identity" "keda" {
+  name                = "keda-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+resource "azurerm_role_assignment" "keda_servicebus_data_owner" {
+  principal_id = data.azurerm_user_assigned_identity.keda.id
+  scope        = module.servicebus-namespace.id
+  role_definition_name = "Azure Service Bus Data Owner"
+}
