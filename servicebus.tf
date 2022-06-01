b0e91be4cb4cb30576dcc8089bb9e74750d879c3
msl8r <matt.slater@hmcts.net>
2022-06-01 12:58:05 +0100

    DTSPO-8169 - fix errors

--- servicebus.tf
+++ servicebus.tf
@@ -16,24 +16,24 @@ module "servicebus-namespace" {
   project                 = var.project
 }
 
-data "azurerm_user_assigned_identity" "toffee_mi" {
+data "azurerm_user_assigned_identity" "toffee" {
   name                = "toffee-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
 resource "azurerm_role_assignment" "toffee_servicebus_data_owner" {
-  principal_id = data.azurerm_user_assigned_identity.toffee_mi
+  principal_id = data.azurerm_user_assigned_identity.toffee.id
   scope        = module.servicebus-namespace.id
   role_definition_name = "Azure Service Bus Data Owner"
 }
 
-data "azurerm_user_assigned_identity" "keda_servicebus_data_owner" {
+data "azurerm_user_assigned_identity" "keda" {
   name                = "keda-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
 }
 
-resource "azurerm_role_assignment" "keda_servicebus" {
-  principal_id = data.azurerm_user_assigned_identity.toffee_mi
+resource "azurerm_role_assignment" "keda_servicebus_data_owner" {
+  principal_id = data.azurerm_user_assigned_identity.keda.id
   scope        = module.servicebus-namespace.id
   role_definition_name = "Azure Service Bus Data Owner"
 }
