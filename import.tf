e6da62408c2d99a8940e44f5abecc1d98e9bd90c
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-12-12 12:35:54 +0000

    update

--- import.tf
+++ import.tf
@@ -4,11 +4,6 @@ import {
 }
 
 import {
-  to = azurerm_monitor_diagnostic_setting.kv-ds
-  id = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/apple-shared-infrastructure-sbox/providers/Microsoft.KeyVault/vaults/applesi-sbox|applesi-sbox"
-}
-
-import {
   to = azurerm_key_vault_access_policy.creator_access_policy
   id = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/apple-shared-infrastructure-sbox/providers/Microsoft.KeyVault/vaults/applesi-sbox/objectId/5356a0e7-324e-4efa-970b-4b4aec3f0ba3"
 }
