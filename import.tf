69299585bbcea4a429a5562725b87570dc6cfd78
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-12-12 13:34:56 +0000

    removing temp import file

--- import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_key_vault_secret.appInsights-InstrumentationKey
-  id = "https://applesi-sbox.vault.azure.net/secrets/appInsights-InstrumentationKey/ed572b2081214188a355db89c04ccb2a"
-}
