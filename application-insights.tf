c1165564c781ab55c3fba3cf8aea1b1a1080c7e9
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-02-05 10:09:33 +0000

    Update application-insights.tf

--- application-insights.tf
+++ application-insights.tf
@@ -1,5 +1,37 @@
+locals {
+  env = var.env == "sandbox" ? "sbox" : var.env
+}
+
+data "azurerm_windows_function_app" "alerts" {
+  provider            = azurerm.private_endpoint
+  name                = "alerts-slack-${local.env}"
+  resource_group_name = "alerts-slack-${local.env}"
+}
+
+data "azurerm_function_app_host_keys" "host_keys" {
+  provider            = azurerm.private_endpoint
+  name                = data.azurerm_windows_function_app.alerts.name
+  resource_group_name = "alerts-slack-${local.env}"
+}
+
+resource "azurerm_monitor_action_group" "action_group" {
+  name                = "${title(var.product)}-${title(var.env)}-Warning-Alerts"
+  resource_group_name = azurerm_resource_group.shared_resource_group.name
+  short_name          = "${var.product}-${local.env}"
+
+  azure_function_receiver {
+    function_app_resource_id = data.azurerm_windows_function_app.alerts.id
+    function_name            = "httpTrigger"
+    http_trigger_url         = "https://${data.azurerm_windows_function_app.alerts.default_hostname}/api/httpTrigger?code=${data.azurerm_function_app_host_keys.host_keys.default_function_key}"
+    name                     = "slack-alerts"
+    use_common_alert_schema  = true
+  }
+
+  tags = var.common_tags
+}
+
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=alert"
 
   env     = var.env
   product = var.product
