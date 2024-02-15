4dff87db4a3edef5e1f73f613e04588e94fe6778
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-02-15 15:57:50 +0000

    testig single action group with toffee

--- application-insights.tf
+++ application-insights.tf
@@ -1,14 +1,14 @@
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=alert"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=DTSPO-16829-prepare"
 
   env     = var.env
   product = var.product
-  name    = "${var.product}"
+  name    = var.product
 
-  resource_group_name   = azurerm_resource_group.shared_resource_group.name
+  resource_group_name = azurerm_resource_group.shared_resource_group.name
   email_receiver_config = {
-    name              = "test"
-    email_address     = "tyler.mcdowell@justice.gov.uk"
+    name          = "test"
+    email_address = "tyler.mcdowell@justice.gov.uk"
   }
   aks_subscription_id = var.aks_subscription_id
 
