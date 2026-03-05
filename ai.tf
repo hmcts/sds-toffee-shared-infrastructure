8357599788474e84d8da132ae085529220f93c81
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2026-03-05 12:34:29 +0000

    Updating Terraform Formatting

--- ai.tf
+++ ai.tf
@@ -5,17 +5,17 @@ data "azurerm_subnet" "private_endpoint_subnet" {
 }
 
 module "ai" {
-  source                          = "git::https://github.com/hmcts/terraform-module-ai-services?ref=main"
-  env                             = var.env
-  project                         = "${var.project}-${var.product}"
-  existing_resource_group_name    = azurerm_resource_group.shared_resource_group.name
-  common_tags                     = var.common_tags
-  product                         = var.project
-  component                       = var.product
-  key_vault_id                    = module.key_vault.key_vault_id
-  application_insights_id         = module.application_insights.id
-  create_cognitive_account        = true
-  cognitive_account_kind          = "SpeechServices"
-  cognitive_account_sku           = "S0"
-  subnet_id                       = data.azurerm_subnet.private_endpoint_subnet.id
+  source                       = "git::https://github.com/hmcts/terraform-module-ai-services?ref=main"
+  env                          = var.env
+  project                      = "${var.project}-${var.product}"
+  existing_resource_group_name = azurerm_resource_group.shared_resource_group.name
+  common_tags                  = var.common_tags
+  product                      = var.project
+  component                    = var.product
+  key_vault_id                 = module.key_vault.key_vault_id
+  application_insights_id      = module.application_insights.id
+  create_cognitive_account     = true
+  cognitive_account_kind       = "SpeechServices"
+  cognitive_account_sku        = "S0"
+  subnet_id                    = data.azurerm_subnet.private_endpoint_subnet.id
 }
