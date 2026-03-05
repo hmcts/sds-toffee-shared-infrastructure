cab163d983fa03bc1de0cb278173c90a0e7114e2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-03-05 12:18:35 +0000

    add ai module

---
+++ ai.tf
@@ -0,0 +1,22 @@
+data "azurerm_subnet" "private_endpoint_subnet" {
+  name                 = "private-endpoints"
+  virtual_network_name = "ss-${var.env}-vnet"
+  resource_group_name  = azurerm_resource_group.shared_resource_group.name
+}
+
+module "ai" {
+  source                          = "git::https://github.com/hmcts/terraform-module-ai-services?ref=main"
+  env                             = var.env
+  project                         = var.product
+  existing_resource_group_name    = azurerm_resource_group.shared_resource_group.name
+  common_tags                     = module.common_tags.common_tags
+  product                         = var.product
+  component                       = var.component
+  key_vault_id                    = module.key_vault.key_vault_id
+  application_insights_id         = module.application_insights.id
+  create_cognitive_account        = true
+  cognitive_account_kind          = "SpeechServices"
+  cognitive_account_sku           = "S0"
+  existing_cognitive_account_name = var.existing_cognitive_account_name
+  subnet_id                       = data.azurerm_subnet.private_endpoint_subnet.id
+}
