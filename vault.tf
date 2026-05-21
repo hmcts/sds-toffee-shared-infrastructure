1dd6c726f19e53c2f08b4549c7906df245dd99c2
Daniel Alves <daniel.alves@justice.gov.uk>
2026-05-21 12:47:45 +0100

    DTSPO-30107: use Jenkins agents library branch

--- vault.tf
+++ vault.tf
@@ -1,7 +1,7 @@
 //KEY VAULT RESOURCE
 
 module "vault" {
-  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                       = local.vault_name
   product                    = var.product
   env                        = var.env
